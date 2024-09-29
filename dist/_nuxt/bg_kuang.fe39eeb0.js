75e18740755d9745f470b8de19d8439be54b1e98
psh006 <2052702730@qq.com>
2024-09-29 11:21:25 +0800

    init

---
+++ dist/_nuxt/bg_kuang.fe39eeb0.js
@@ -0,0 +1,1 @@
+import"./entry.5e3e9910.js";const g=""+globalThis.__publicAssetsURL("images/login/bg_kuang.png?v=1");export{g as _};
--- server/chunks/rollup/_virtual_head-static.mjs
+++
@@ -1,3 +0,0 @@
-const _virtual__headStatic = {"headTags":"<meta charset=\"utf-8\">\n<title>Gaea</title>\n<meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">\n<meta name=\"description\" content=\"\">\n<meta name=\"format-detection\" content=\"telephone=no\">\n<meta name=\"viewport\" content=\"user-scalable=yes\">\n<meta name=\"naive-ui-style\">\n<meta name=\"vueuc-style\">\n<link rel=\"icon\" type=\"image/x-icon\" href=\"/favicon.ico?v=1\">","bodyTags":"","bodyTagsOpen":"","htmlAttrs":"","bodyAttrs":""};
-
-export { _virtual__headStatic as default };

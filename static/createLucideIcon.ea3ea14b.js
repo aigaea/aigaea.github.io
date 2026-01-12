3e20ce68bfc52a477bd867cc1b5a29a34ccb9e62
gaea3 <develop@aigaea.net>
2026-01-12 10:31:33 +0000

    Initialize language pack

---
+++ static/createLucideIcon.ea3ea14b.js
@@ -0,0 +1,19 @@
+const _0x2c84d5=_0x5ba3;function _0x292f(){const _0x2b950a=['6QMVgVO','2680dMmCQy','16pRabFl','none','16125scOCpZ','replace','2265570wSiXnN','lucide-','lucide-icon','21020JVEyMo','76388bpNsyE','class','join','9gwQRPh','894eNbKYp','lucide','282171rYsiMa','5159EzmkXK','http://www.w3.org/2000/svg','13643xMjVyY','1568HVYntG','24468OyXXnD'];_0x292f=function(){return _0x2b950a;};return _0x292f();}(function(_0x44cfc5,_0x55d2f0){const _0x33c1f6=_0x5ba3,_0x5ae8cd=_0x44cfc5();while(!![]){try{const _0x302133=parseInt(_0x33c1f6(0x187))/0x1*(parseInt(_0x33c1f6(0x194))/0x2)+parseInt(_0x33c1f6(0x18e))/0x3*(parseInt(_0x33c1f6(0x196))/0x4)+parseInt(_0x33c1f6(0x198))/0x5*(-parseInt(_0x33c1f6(0x18c))/0x6)+-parseInt(_0x33c1f6(0x191))/0x7*(-parseInt(_0x33c1f6(0x195))/0x8)+-parseInt(_0x33c1f6(0x18b))/0x9*(-parseInt(_0x33c1f6(0x19a))/0xa)+-parseInt(_0x33c1f6(0x18f))/0xb*(parseInt(_0x33c1f6(0x193))/0xc)+parseInt(_0x33c1f6(0x188))/0xd*(parseInt(_0x33c1f6(0x192))/0xe);if(_0x302133===_0x55d2f0)break;else _0x5ae8cd['push'](_0x5ae8cd['shift']());}catch(_0x5971a7){_0x5ae8cd['push'](_0x5ae8cd['shift']());}}}(_0x292f,0x83d98));function _0x5ba3(_0x4e361b,_0x58e271){_0x4e361b=_0x4e361b-0x186;const _0x292f03=_0x292f();let _0x5ba33f=_0x292f03[_0x4e361b];return _0x5ba33f;}import{an as _0x49acb2}from'./entry.c15994d8.js';/**
+ * @license lucide-vue-next v0.555.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+const w=_0x427001=>_0x427001[_0x2c84d5(0x199)](/([a-z0-9])([A-Z])/g,'$1-$2')['toLowerCase'](),g=_0x8d87cb=>_0x8d87cb['replace'](/^([A-Z])|[\s-_]+(\w)/g,(_0x474a0f,_0xe5f909,_0x5355a2)=>_0x5355a2?_0x5355a2['toUpperCase']():_0xe5f909['toLowerCase']()),p=_0x35e493=>{const _0x4cafc2=g(_0x35e493);return _0x4cafc2['charAt'](0x0)['toUpperCase']()+_0x4cafc2['slice'](0x1);},C=(..._0x34d332)=>_0x34d332['filter']((_0x448d26,_0x2a0a5d,_0x41d9c8)=>!!_0x448d26&&_0x448d26['trim']()!==''&&_0x41d9c8['indexOf'](_0x448d26)===_0x2a0a5d)[_0x2c84d5(0x18a)]('\x20')['trim'](),d=_0x596a02=>_0x596a02==='';/**
+ * @license lucide-vue-next v0.555.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+var s={'xmlns':_0x2c84d5(0x190),'width':0x18,'height':0x18,'viewBox':'0\x200\x2024\x2024','fill':_0x2c84d5(0x197),'stroke':'currentColor','stroke-width':0x2,'stroke-linecap':'round','stroke-linejoin':'round'};/**
+ * @license lucide-vue-next v0.555.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+const f=({name:_0xe05184,iconNode:_0x1b99e2,absoluteStrokeWidth:_0x1c6778,'absolute-stroke-width':_0xb9c68d,strokeWidth:_0x531410,'stroke-width':_0x3b3510,size:_0x3347b1=s['width'],color:_0x3b81af=s['stroke'],..._0x2f42d4},{slots:_0x3a135c})=>_0x49acb2('svg',{...s,..._0x2f42d4,'width':_0x3347b1,'height':_0x3347b1,'stroke':_0x3b81af,'stroke-width':d(_0x1c6778)||d(_0xb9c68d)||_0x1c6778===!0x0||_0xb9c68d===!0x0?Number(_0x531410||_0x3b3510||s['stroke-width'])*0x18/Number(_0x3347b1):_0x531410||_0x3b3510||s['stroke-width'],'class':C(_0x2c84d5(0x18d),_0x2f42d4[_0x2c84d5(0x189)],..._0xe05184?[_0x2c84d5(0x19b)+w(p(_0xe05184))+'-icon','lucide-'+w(_0xe05184)]:[_0x2c84d5(0x186)])},[..._0x1b99e2['map'](_0x3eb231=>_0x49acb2(..._0x3eb231)),..._0x3a135c['default']?[_0x3a135c['default']()]:[]]),v=(_0x40af8a,_0x2cb99b)=>(_0x5b2f6b,{slots:_0x5abc47,attrs:_0x4a10e4})=>_0x49acb2(f,{..._0x4a10e4,..._0x5b2f6b,'iconNode':_0x2cb99b,'name':_0x40af8a},_0x5abc47);export{v as c};
